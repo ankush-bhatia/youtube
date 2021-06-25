64dc68e1092f802227a01ecfd24841dddeb77996
Ankush Bhatia <ankush@dunzo.in>
2021-06-25 15:09:24 +0530

    Added VariablesAndDataTypes project.

---
+++ Swift Course/VariablesAndDataTypes/VariablesAndDataTypes/main.swift
@@ -0,0 +1,58 @@
+//
+//  main.swift
+//  VariablesAndDataTypes
+//
+//  Created by Ankush Bhatia on 25/06/21.
+//
+
+import Foundation
+
+// Variable
+//var name = "Ankush"
+//print(name)
+//
+//name = "Bhatia"
+//print(name)
+
+// Constant
+//let name = "Ankush"
+//name = "Bhatia"
+
+// Type Annotation
+//var name: String = "Ankush"
+
+// Printing
+//print(name)
+//print("My name is: \(name)")
+
+// Data Types
+//1. Int
+
+//let value: Int = 2
+
+// Floating
+// Float
+// Double
+
+//let value: Float = 2.0
+
+// String
+
+// Boolean
+// true or false
+//var isRunning: Bool? = false
+
+// Optional
+//var name: Optional<String> = "Ankush"
+
+//var value: Int? = 4
+//var name: String? = "Ankush"
+
+// Printing
+//print(name!)
+//print("My name is: \(String(describing: name))")
+
+// Tuples
+let person = (name: "Ankush", exp: 4)
+print(person.name)
+print(person.exp)
